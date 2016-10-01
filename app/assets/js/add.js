00fa617635c69352a8f9b6e62ff5991dcf99fa9e
Yury Samakhvalau <mr.george_s@mail.ru>
2016-10-01 20:36:05 +0000

    add page - add news

--- app/assets/js/add.js
+++ app/assets/js/add.js
@@ -11,3 +11,69 @@ app.currentModule = (function($){
         }
     }
 })(jQuery);
+
+
+/*
+(function(){
+    //console.log('yep');
+    
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.backendless.com/v1/data/category',
+        headers: {"application-id" : "14469D9E-0DF1-09B9-FF27-2739263FE500", "secret-key" : "2EA4DAF8-A0D4-6A10-FF50-19F884C04800"},
+        dataType: 'json',
+        beforeSend: function() {},
+        success: function(data){
+            var option,
+                itemData = data.data;
+            
+            //console.log(data.data);
+            
+            for(var i = 0; itemData.length > i; i++){
+                //console.log(data.data[i]);
+                
+                option += '<option value="' + itemData[i].objectId + '">' + itemData[i].name + '</option>';
+            }
+            
+            $('#categoryId').html(option);
+          
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+          alert(xhr.status);
+          alert(thrownError);
+        },
+        complete: function() {}
+        
+    });
+    
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.backendless.com/v1/data/type',
+        headers: {"application-id" : "14469D9E-0DF1-09B9-FF27-2739263FE500", "secret-key" : "2EA4DAF8-A0D4-6A10-FF50-19F884C04800"},
+        dataType: 'json',
+        beforeSend: function() {},
+        success: function(data){
+            var option,
+                itemData = data.data;
+            
+            //console.log(data.data);
+            
+            for(var i = 0; itemData.length > i; i++){
+                //console.log(data.data[i]);
+                
+                option += '<option value="' + itemData[i].objectId + '">' + itemData[i].name + '</option>';
+            }
+            
+            $('#type').html(option);
+          
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+          alert(xhr.status);
+          alert(thrownError);
+        },
+        complete: function() {}
+        
+    });
+    
+    
+})()*/

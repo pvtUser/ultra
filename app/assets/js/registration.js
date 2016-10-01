a2c6279fcab9a82172921e6de13c6cb880307d1d
Eugene <evgenktulu@mail.ru>
2016-10-01 20:10:27 +0000

    registration and login forms added

--- app/assets/js/registration.js
+++ app/assets/js/registration.js
@@ -1,4 +1,4 @@
-app.currentModule = (function($){
+app.currentModule = (function($) {
     return {
         init: function(obj, callback) {
             console.log("Инициализируем модуль для регистрации");
@@ -6,7 +6,27 @@ app.currentModule = (function($){
             callback = callback || function() {
                 return false;
             }
-            
+            $(document).ready(function() {
+
+                $('#registration_button').on('click', function() {
+                    var user = new Backendless.User();
+                    user.email = $('#email').val();
+                    user.password = $('#password').val();
+                    user.name = $('#firstName').val();
+
+                    try {
+                        Backendless.UserService.register(user);
+                        $('#output').prepend('<div class="alert alert-success alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Вы успешно зарегистрировались!</strong></div>');
+                        $('a#login').replaceWith('<a class="nav-link" id="logout" href="#logout">logout</a>');
+                    }
+                    catch (err) {
+                        console.log("error message - " + err.message);
+                        console.log("error code - " + err.statusCode);
+                        $('#output').prepend('<div class="alert alert-success alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Проверьте правильность введенных данных!</strong></div>');
+                    }
+                });
+            });
+
             callback();
         }
     }

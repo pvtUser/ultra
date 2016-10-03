2867dbb5237202320e550196bb12307e9d15609c
Eugene <evgenktulu@mail.ru>
2016-10-03 07:24:44 +0000

    index.js removed comments

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -7,15 +7,6 @@ app.currentModule = (function($) {
                 return false;
             };
 
-            // $(document).ready(function() {
-            //     $('.bxslider').bxSlider();
-            // });
-
-            // var savedPoster = Backendless.Persistence.of('poster');
-            // console.log(savedPoster);
-            // var img = Backendless.Persistence.of('poster').findById('B9A75661-CB5A-2EED-FF26-FC60794C9800');
-            // console.log(img);
-
             callback();
         }
     }

24e4ee10e1846526f13967093bdcd3319b70dfe5
Yury Samakhvalau <mr.george_s@mail.ru>
2016-10-03 07:21:56 +0000

    тестоый вывод записей

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -3,11 +3,43 @@ app.currentModule = (function($){
         init: function(obj, callback) {
             console.log("Инициализируем модуль для главной страницы");
             obj = obj || new Object(null);
+            
+            /*
             callback = callback || function() {
                 return false;
             }
+            callback();*/
             
-            callback();
+            var getPosts = function(){
+                
+                var resultHtml = '',
+                    posts = Backendless.Persistence.of('poster').find();
+                
+                for(var i = 0; i < posts.data.length; i++){
+                    console.log(posts.data[i]);
+                    resultHtml += '<h4>' + posts.data[i]['title'] + '</h4>';
+                }
+                
+                //console.log(resultHtml);
+                
+                $(obj).find('#all-news').html(resultHtml);
+                
+                
+                
+                
+                /*var search = window.location.search.substr(1),
+	            keys = {};
+      
+                search.split('&').forEach(function(item) {
+                	item = item.split('=');
+                	keys[item[0]] = item[1];
+                });
+      
+                console.log(Object.keys(keys));*/
+
+            }
+            
+            getPosts();
         }
     }
 })(jQuery);

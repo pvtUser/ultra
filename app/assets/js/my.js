796ee438b00cf762f9197b9ff66b1a506cf2bf44
Eugene <evgenktulu@mail.ru>
2016-10-04 07:31:41 +0000

    добавлен вывод объявлений конкретного пользователя на страницу Мои объявления. Решена проблема с правильным выводом полного объявления

---
+++ app/assets/js/my.js
@@ -0,0 +1,39 @@
+app.currentModule = (function($) {
+    return {
+        init: function(obj, callback) {
+            console.log("Инициализируем модуль для выхода");
+            obj = obj || new Object(null);
+            callback = callback || function() {
+                return false;
+            };
+
+            var getMyPosts = function() {
+
+                var resultHtml = '';
+                var currentUser = Backendless.UserService.getCurrentUser();
+                console.log(currentUser);
+
+                var posts = Backendless.Persistence.of('poster').find();
+                for (var i = 0; i < posts.data.length; i++) {
+                    if (currentUser['ownerId'] == posts.data[i]['ownerId']) {
+                        resultHtml += '<div class="col-md-4 card"><div class="card-block"><h4 class="card-title">' +
+                            posts.data[i]['title'] +
+                            '</h4></div><img src="' +
+                            posts.data[i]['image'] +
+                            '"><div class="card-block"><p class="card-text">Цена: ' +
+                            posts.data[i]['price'] +
+                            '</p></div><a href="#" data-toggle="modal" data-target="#myModal' + i + '">Показать полное описание</a></div>' +
+                            '<div class="modal fade" id="myModal' + i + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">Полное описание</h4></div><div class="modal-body">' +
+                            posts.data[i]['fullDescription'] +
+                            '</div></div></div></div>';
+                    }
+                }
+                $(obj).find('#my-news').html(resultHtml);
+            }
+
+            getMyPosts();
+
+            callback();
+        }
+    }
+})(jQuery);
